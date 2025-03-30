dc429b91140efd77e887ddb838fefeecadde4643
Pskyto03 <166969265+Pskyto03@users.noreply.github.com>
2025-03-30 11:43:27 -0500

    first commit

---
+++ src/main/java/com/example/productos/repository/ProductoRepository.java
@@ -0,0 +1,6 @@
+package com.example.productos.repository;
+import com.example.productos.model.Producto;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+public interface ProductoRepository extends ReactiveCrudRepository<Producto, String> 
+{
+}
